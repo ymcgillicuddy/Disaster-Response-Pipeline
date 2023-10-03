de69851119a4eab9e637e815e6b84b897d357b28
ymcgillicuddy <137903104+ymcgillicuddy@users.noreply.github.com>
2023-10-03 16:05:38 +0100

    update clean_data function in process_data.py

--- process_data.py
+++ process_data.py
@@ -12,10 +12,37 @@ def load_data(messages_filepath, categories_filepath):
     messages = pd.read_csv(messages_filepath, encoding='latin-1')
     categories = pd.read_csv(categories_filepath, encoding='latin-1')
     df = messages.merge(categories, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    Reads in dataframe
+    Parses compound categories column into 36 binary columns
+    Joins the binary columns back onto the dataframe; drops the original "categories" column
+    Drops non-binary records and duplicates
+    Returns cleansed dataframe
+    '''
+    categories = df['categories'].str.split(';',n=36, expand = True) 
+    #split string on semi-colon# create a dataframe of the 36 individual category columns
+
+    row = categories.iloc[0] # select the first row of the categories dataframe
+
+    category_colnames = row[lambda x : x.str.slice(0,-2)] 
+    categories.columns = category_colnames.index
+    #slice off last 2 characters from first row to generate column headers
+
+    for column in categories:
+        categories[column] = categories[column].str.slice(-1,)
+        categories[column] = pd.to_numeric(categories[column])
+        # take the last character of the string as the value and ensure values are numerical
+
+    df = df.drop(['categories'], axis=1) # drop the original categories column from `df`
+    df = pd.concat([df,categories], axis=1) # concatenate the original dataframe with the new `categories` dataframe
+    df = df.drop(df[df['related'] == 2].index) #drop some rows with "2" in related
+    df = df.drop(['child_alone'], axis=1) #drop this column as it had zero 1 values so can't be used for prediction model
+    df = df.drop_duplicates() # drop duplicates   
+    return df
 
 
 def save_data(df, database_filename):
